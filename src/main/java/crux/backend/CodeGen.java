01be593bba2a7f01df97fd1d2ffae4dfd2ee4b40
Tzu Hsuan Huang(Alyssa) <117048389+allyer88@users.noreply.github.com>
2024-05-26 21:55:42 -0700

    finish private/public genCode

--- src/main/java/crux/backend/CodeGen.java
+++ src/main/java/crux/backend/CodeGen.java
@@ -1,11 +1,15 @@
 package crux.backend;
 
 import crux.ast.SymbolTable.Symbol;
+import crux.ast.types.BoolType;
+import crux.ast.types.IntType;
+import crux.ast.types.Type;
 import crux.ir.*;
 import crux.ir.insts.*;
 import crux.printing.IRValueFormatter;
 
 import java.util.*;
+
 
 /**
  * Convert the CFG into Assembly Instructions
@@ -25,10 +29,90 @@ public final class CodeGen extends InstVisitor {
   /**
    * It should allocate space for globals call genCode for each Function
    */
+  private long getSize(IntegerConstant i, Type t){
+    if(t.getClass()== IntType.class){
+      return i.getValue()*4;
+    }
+    //t.getClass() == BoolType.class
+    return i.getValue();//*1byte
+  }
   public void genCode() {
-    //TODO
-    
+    for (Iterator<GlobalDecl> glob_it = p.getGlobals(); glob_it.hasNext();){
+      GlobalDecl g = glob_it.next();
+      Symbol symbol = g.getSymbol();
+      String name = symbol.getName();
+      long size = getSize(g.getNumElement(), symbol.getType());
+      out.printCode(".comm" + name + ", " + size + ", 8" );
+    }
+    int count[] = new int[1];
+    for(Iterator<Function> fun_it = p.getFunctions(); fun_it.hasNext();){
+      Function f = fun_it.next();
+      genCode(f, count);
+    }
     out.close();
+  }
+  HashMap<Variable, Integer> varMap = new HashMap<Variable, Integer>();
+  int varcount = 0;
+
+  public int numSlots() {
+    return varcount;
+  }
+  Integer getStackSlot(Variable v) {
+    if (!varMap.containsKey(v)) {
+      varcount++;
+      varMap.put(v, varcount);
+    }
+    return varMap.get(v);
+  }
+  HashMap<Instruction, String> InstMap =new HashMap<Instruction, String>();
+
+  private void visitBody(Instruction i){
+    //Linearize CFG using jumps and labels
+    //Use DFS traversal
+    //Refer to Function.assignLabels(int count[])
+    if(i==null){
+      return;
+    }
+    i.accept(this);
+    i = i.getNext(0);
+    if(i==null){
+      i = i.getNext(1);
+    }
+    visitBody(i);
+  }
+  private void genCode(Function f, int count[]){
+    //Assign labels to jump targets f.assignLabels(count);
+    //then store it in a global variable
+    HashMap<Instruction, String> var = f.assignLabels(count);
+    InstMap.putAll(var);
+    out.printCode(".globl " + f.getName());
+    out.printLabel(f.getName() + ":");
+    //Print prologue such that stack is 16 byte aligned
+    int numSlots =  f.getNumTempVars()+f.getNumTempAddressVars();
+    numSlots = (numSlots + 1) & ~1; //round up to nearest even number
+    out.printCode("enter $(8 * " + numSlots + "), $0");
+    //Move arguments from registers to local variable
+    List<String> argReg = Arrays.asList("%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9");
+    List<LocalVar> args= f.getArguments();
+    for(int i=0;i<args.size();i++){
+      int offset;
+      int varSlots = getStackSlot(args.get(i));
+      if(i<6){
+        offset = -varSlots * 8;
+        out.printCode("movq "+ argReg.get(i) +", " + offset + "(%rbp)");
+      }else{
+        int argRegOffset = 16 * (i-5);
+        offset = -varSlots * 8;
+        out.printCode("movq "+ argRegOffset + "(%rbp), " + offset + "(%rbp)");
+      }
+    }
+    //Generate code for function body
+    Instruction i = f.getStart();
+    visitBody(i);
+    //Print epilogue
+    out.printCode("movq $0, %rax");
+    out.printCode("leave");
+    out.printCode("ret");
   }
 
   public void visit(AddressAt i) {}
