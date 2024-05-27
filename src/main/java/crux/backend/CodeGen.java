5426910ef2f0d83f76db0dc546b21c9ca7014742
Tzu Hsuan Huang(Alyssa) <117048389+allyer88@users.noreply.github.com>
2024-05-27 11:48:25 -0700

    implement some visit

--- src/main/java/crux/backend/CodeGen.java
+++ src/main/java/crux/backend/CodeGen.java
@@ -1,6 +1,7 @@
 package crux.backend;
 
 import crux.ast.SymbolTable.Symbol;
+import crux.ast.types.ArrayType;
 import crux.ast.types.BoolType;
 import crux.ast.types.IntType;
 import crux.ast.types.Type;
@@ -66,19 +67,26 @@ public final class CodeGen extends InstVisitor {
   }
   HashMap<Instruction, String> InstMap =new HashMap<Instruction, String>();
 
-  private void visitBody(Instruction i){
+  private void visitBody(Instruction i, Function f, int count[]){
     //Linearize CFG using jumps and labels
     //Use DFS traversal
     //Refer to Function.assignLabels(int count[])
-    if(i==null){
-      return;
-    }
-    i.accept(this);
-    i = i.getNext(0);
-    if(i==null){
+    //Keep track of visited instructions in a set to avoid redundant visits
+    while(i!=null){
+      if(InstMap.containsKey(i)){
+        out.printCode("jmp " + InstMap.get(i));
+      }else if(i.getClass() == JumpInst.class){
+        HashMap<Instruction, String> var = f.assignLabels(count);
+        InstMap.putAll(var);
+        i.accept(this);
+      }else {
+        i.accept(this);
+      }
+      i = i.getNext(0);
+      visitBody(i,f ,count);
       i = i.getNext(1);
     }
-    visitBody(i);
+
   }
   private void genCode(Function f, int count[]){
     //Assign labels to jump targets f.assignLabels(count);
@@ -108,30 +116,135 @@ public final class CodeGen extends InstVisitor {
     }
     //Generate code for function body
     Instruction i = f.getStart();
-    visitBody(i);
+    visitBody(i,f, count);
     //Print epilogue
     out.printCode("movq $0, %rax");
     out.printCode("leave");
     out.printCode("ret");
   }
 
-  public void visit(AddressAt i) {}
+  public void visit(AddressAt i) {
+    out.printCode("/* AddressAt */");
+    LocalVar varOffset = i.getOffset();
+    Symbol symbol = i.getBase();
+    String name = symbol.getName();
+    int varCount = getStackSlot(i.getDst());
+    int varStackOffset = -varCount*8;
+    out.printCode("movq "+ name+ "@GOTPCREL(%rip) , %r11");
+    out.printCode("movq %r11, " + varStackOffset + "(%rbp)");
+    if(varOffset!=null){
+      int offsetCount = getStackSlot(varOffset);
+      int stackOffset = -offsetCount*8;
+      out.printCode("movq " + stackOffset + "(%rbp), %r11" );// Load offset
+      out.printCode("movq $8, %r10");
+      out.printCode("imul %r10, %r11");// Multiply offset by 8
 
-  public void visit(BinaryOperator i) {}
+      movq Data@GOTPCREL(%rip) , %r10     // Load array address
+      addq %r10, %r11                     // Add array base address with offset
+      movq %r11, -368(%rbp)
+    }
+  }
 
-  public void visit(CompareInst i) {}
+  public void visit(BinaryOperator i) {
+    Variable dst = i.getDst();
+    Variable lhs = i.getLeftOperand();
+    Variable rhs = i.getRightOperand();
+    int dstslot = getStackSlot(dst);
+    int lhsslot = getStackSlot(lhs);
+    int rhsslot = getStackSlot(rhs);
+    int dstoffset = -dstslot * 8;
+    int lhsoffset = -lhsslot * 8;
+    int rhsoffset = -rhsslot * 8;
+    out.printCode("movq "+lhsoffset+"(%rbp), %r10");
+    switch(i.getOperator()) {
+      case Add:
+        out.printCode("addq "+rhsoffset+"(%rbp), %r10");
+        out.printCode("movq %r10, "+ dstoffset+"(%rbp)");
+        break;
+      case Sub:
+        out.printCode("subq "+rhsoffset+"(%rbp), %r10");
+        out.printCode("movq %r10, "+ dstoffset+"(%rbp)");
+        break;
+      case Mul:
+        out.printCode("imulq "+rhsoffset+"(%rbp), %r10");
+        out.printCode("movq %r10, "+ dstoffset+"(%rbp)");
+        break;
+      case Div:
+        out.printCode("cqto");
+        out.printCode("idivq " + rhsoffset + "(%rbp)");
+        out.printCode("movq %rax, "+ dstoffset+"(%rbp)");
+        break;
+    }
+  }
 
-  public void visit(CopyInst i) {}
+  public void visit(CompareInst i) {
+    Variable dst = i.getDst();
+    Variable lhs = i.getLeftOperand();
+    Variable rhs = i.getRightOperand();
+    int dstslot = getStackSlot(dst);
+    int lhsslot = getStackSlot(lhs);
+    int rhsslot = getStackSlot(rhs);
+    int dstoffset = -dstslot * 8;
+    int lhsoffset = -lhsslot * 8;
+    int rhsoffset = -rhsslot * 8;
+    out.printCode("movq $0, %rax");//false
+    out.printCode("movq $1, %r10");//true
+    out.printCode("movq "+lhsoffset+"(%rbp), %r11");
+    out.printCode("cmp "+rhsoffset+"(%rbp), %r11"); //lfs-rhs
+    switch(i.getPredicate()){
+      case GE:
+        out.printCode("cmovge %r10, %rax");
+        break;
+      case GT:
+        out.printCode("cmovg %r10, %rax");
+        break;
+      case LE:
+        out.printCode("cmovle %r10, %rax");
+        break;
+      case LT:
+        out.printCode("cmovl %r10, %rax");
+        break;
+      case NE:
+        out.printCode("cmovne %r10, %rax");
+        break;
+      case EQ:
+        out.printCode("cmove %r10, %rax");
+        break;
+    }
+    out.printCode("movq %rax, "+ dstoffset+"(%rbp)");
+  }
+
+  public void visit(CopyInst i) {
+    out.printCode("/* CopyInst */");
+    Variable dst = i.getDstVar();
+    Value scr = i.getSrcValue();
+    int dstslot = getStackSlot(dst);
+    int dstoffset = -dstslot * 8;
+    out.printCode("movq " + scr + "%r10" );
+    out.printCode("movq "+dstoffset+"(%rbp), %r11");
+
+  }
 
   public void visit(JumpInst i) {}
 
   public void visit(LoadInst i) {}
 
-  public void visit(NopInst i) {}
+  public void visit(NopInst i) {
+    out.printCode("/* Nop */");
+  }
 
   public void visit(StoreInst i) {}
 
-  public void visit(ReturnInst i) {}
+  public void visit(ReturnInst i) {
+    LocalVar returnValue = i.getReturnValue();
+    int retslot = getStackSlot(returnValue);
+    int retoffset = -retslot * 8;
+    //we need to make sure, that the value we want to return is in register %rax
+    // and then return from the function
+    out.printCode("movq " + retoffset + "(%rbp), %rax");
+    out.printCode("leave");
+    out.printCode("ret");
+  }
 
   public void visit(CallInst i) {}
 
